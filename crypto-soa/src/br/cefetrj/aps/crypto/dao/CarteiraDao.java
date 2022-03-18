91b9f19b4c8eead35279294de471d04209335e19
Gabrel-Maga-23 <65359036+Gabrel-Maga-23@users.noreply.github.com>
2022-03-18 10:56:25 -0300

    Update CarteiraDao.java

--- crypto-soa/src/br/cefetrj/aps/crypto/dao/CarteiraDao.java
+++ crypto-soa/src/br/cefetrj/aps/crypto/dao/CarteiraDao.java
@@ -17,19 +17,40 @@ public class CarteiraDao // D.A.O = Data Access Object
 	static
 	{
 		// Cria um ativo
-		Ativo     ltc = new Ativo("LTC",0,0,0);
-		// Cria uma transa��o
+		Ativo     ltc = new Ativo("LTC",0,0,0,DateUtils.parse("2089-02-04 11:00:00"));
+		// Cria uma transação
 		Transacao tx1 = new Transacao(ltc, true, 0.1, 230, DateUtils.parse("2022-02-04 11:00:00"));
 		// Cria uma carteira
 		Carteira carteira = new Carteira();
 		carteira.setId("WALLET");
 		
-		// Processa a transa��o na carteira
+		// Processa a transação na carteira
 		CarteiraService carteiraService = new CarteiraService();
 		carteiraService.processarTransacao(carteira,tx1);
 		
 		// Salva a carteira no banco de dados
 		salvar(carteira);
+		
+		
+	}
+	static
+	{
+		// Cria um ativo
+		Ativo     rtc = new Ativo("RTC",0,0,0,DateUtils.parse("2022-02-04 11:00:00"));
+		// Cria uma transação
+		Transacao tx2 = new Transacao(rtc, true, 0.1, 230, DateUtils.parse("2078-02-04 11:00:00"));
+		// Cria uma carteira
+		Carteira carteira2 = new Carteira();
+		carteira2.setId("WALLET2");
+		
+		// Processa a transação na carteira
+		CarteiraService carteiraService2 = new CarteiraService();
+		carteiraService2.processarTransacao(carteira2,tx2);
+		
+		// Salva a carteira no banco de dados
+		salvar2(carteira2);
+		
+		
 	}
 	
 	public static void salvar(Carteira carteira) 
@@ -52,4 +73,15 @@ public class CarteiraDao // D.A.O = Data Access Object
 		
 		return null;
 	}
+	public static void salvar2(Carteira carteira2) 
+	{
+		carteiras.add(carteira2);
+	}
+	
+	public static void remover2(Carteira carteira2)
+	{
+		carteiras.remove(carteira2);
+	}
+	
+	
 }
